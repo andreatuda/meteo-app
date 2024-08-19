0fa0c57983136c5e68bca195f8e2076a514cf001
your-name-hereandreatudja <andreatudja7@gmail.com>
2024-08-19 18:58:29 +0200

    Added the search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "49b2200281d0fo13a5bd3a8b562at5fe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+
+  // make api call and update UI
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
+
+// on load of the page by default, temperature of Krk will be displayed
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Krk");
